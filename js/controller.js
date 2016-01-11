2f9e8907c03795efbbf7feea194668b59c07a801
corand <andoni.correas@gmail.com>
2016-01-11 10:08:49 +0100

    argazkien carousela

--- js/controller.js
+++ js/controller.js
@@ -1,27 +1,46 @@
-define(['js/app', 'backbone', 'marionette', 'js/views/Menua/MenuaView', 'js/views/Egutegia/EgutegiaView', 'js/views/Albisteak/AlbisteakView', 'js/views/Argazkiak/ArgazkiakView'],
-    function (App, Backbone, Marionette, MenuaView, EgutegiaView, AlbisteakView, ArgazkiakView) {
+define(['js/app', 'backbone', 'marionette', 'js/views/Menua/MenuaView', 'js/views/Egutegia/EgutegiaView', 'js/views/Albisteak/AlbisteakView', 'js/views/Argazkiak/ArgazkiakView', 'js/views/Argazkiak/ArgazkiCollection'],
+    function (App, Backbone, Marionette, MenuaView, EgutegiaView, AlbisteakView, ArgazkiakView,ArgazkiCollection) {
     return Backbone.Marionette.Controller.extend({
         initialize:function (options) {
             //App.headerRegion.show(new HeaderView());
             App.sidebarRegion.show(new MenuaView());
         },
+        clear: function(){
+            clearTimeout(this.argazki_carousel);
+            $(".nav-pills li").removeClass("active");
+        },
         //gets mapped to in AppRouter's appRoutes
         egutegia:function () {
-            $(".nav-pills li").removeClass("active");
+            this.clear();
             $("#egutegia").addClass("active");
             App.mainRegion.show(new EgutegiaView());
         },
 
         albisteak: function() {
-            $(".nav-pills li").removeClass("active");
+            this.clear();
             $("#albisteak").addClass("active");
             App.mainRegion.show(new AlbisteakView());
         },
 
         argazkiak: function() {
-            $(".nav-pills li").removeClass("active");
+            this.clear();
             $("#argazkiak").addClass("active");
-            App.mainRegion.show(new ArgazkiakView());
+            var argazki_collection,
+                i=0,
+                timestamp = Date.now();
+                $.getJSON("js/views/Argazkiak/argazkiak.json?v="+timestamp, function(argazkiak) {
+                    argazki_collection = new ArgazkiCollection(argazkiak, { view: this });
+                    App.mainRegion.show(new ArgazkiakView({
+                        model: argazki_collection.at(i)
+                    }));
+                });
+                this.argazki_carousel = setInterval(function(){
+                    i = (i >= argazki_collection.length-1) ? 0 : i+=1;
+                    console.log(i);
+                    App.mainRegion.show(new ArgazkiakView({
+                        model: argazki_collection.at(i)
+                    }));
+                },5000);
         }
     });
 });
--- js/views/Argazkiak/ArgazkiakView.js
+++ js/views/Argazkiak/ArgazkiakView.js
@@ -1,17 +1,10 @@
-define( [ 'js/app', 'marionette', 'handlebars', 'js/views/Argazkiak/ArgazkiCollection', 'text!js/views/Argazkiak/argazkiak.html'],
-    function( App, Marionette, Handlebars, ArgazkiCollection, template) {
+define( [ 'js/app', 'marionette', 'handlebars', 'text!js/views/Argazkiak/argazkiak.html'],
+    function( App, Marionette, Handlebars, template) {
         //ItemView provides some default rendering logic
         return Marionette.ItemView.extend( {
             //Template HTML string
             template: Handlebars.compile(template),
 
-            onRender: function(){
-
-                $.getJSON("js/views/Argazkiak/argazkiak.json?v=12", function(argazkiak) {
-                  console.log(argazkiak);
-                });
-                
-            },
 
             // View Event Handlers
             events: {
